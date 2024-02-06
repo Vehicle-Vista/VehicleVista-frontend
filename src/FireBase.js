785f63485c2457bc6f7c97cc5c279750ab8fce30
Saad Ahmed <saadahmed4324@gmail.com>
2024-02-06 12:01:09 +0500

    added google authentication functionality

---
+++ src/FireBase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAbnPWHNmhWiOAdxCL5-mVw_emBs0plFhk",
+  authDomain: "vehicle-vista-7b565.firebaseapp.com",
+  projectId: "vehicle-vista-7b565",
+  storageBucket: "vehicle-vista-7b565.appspot.com",
+  messagingSenderId: "543916168241",
+  appId: "1:543916168241:web:7945cc3933afc6ca077110",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
