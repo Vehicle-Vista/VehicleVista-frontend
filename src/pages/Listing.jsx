8b9b70a81390f977a1e0e07404021793ac858174
Saad Ahmed <saadahmed4324@gmail.com>
2024-02-18 19:20:06 +0500

    fixed swiper issue

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -2,8 +2,8 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore from "swiper";
-
 import { Navigation } from "swiper/modules";
+import "swiper/css/bundle";
 
 export default function Listing() {
   SwiperCore.use([Navigation]);
